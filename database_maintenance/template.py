51f2f09cae74a3f3b4fb262e3c6adb95c8b45003
steveoh <sgourley@utah.gov>
2016-06-21 17:09:54 -0600

    add arcpy to imports

--- database_maintenance/template.py
+++ database_maintenance/template.py
@@ -9,6 +9,7 @@ A module that contains a template for database maintenance pallets
 from forklift.models import Pallet
 from os.path import join
 from traceback import format_exc
+import arcpy
 
 
 class TemplatePallet(Pallet):
